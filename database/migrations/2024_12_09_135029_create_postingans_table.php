69333186a5160b4fb8dfd65d85393099f57845a5
AhmadFahmy021 <ahmadfahmy219@gmail.com>
2024-12-09 20:59:49 +0700

    first commit

---
+++ database/migrations/2024_12_09_135029_create_postingans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('postingans', function (Blueprint $table) {
+            $table->id();
+            $table->string("foto");
+            $table->foreignId("user_id")->constrained();
+            $table->integer("like");
+            $table->text("description");
+            // $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('postingans');
+    }
+};
